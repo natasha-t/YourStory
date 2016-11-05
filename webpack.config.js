cef3f0df3a4a3a184b19a77b1d9a6faaad954a77
melbee <melba@transferhero.com>
2016-11-04 20:04:11 -0700

    [docs] updated file tree

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-	entry: './client/app.js',
-	output: {
-		path: path.join(__dirname, 'client/app'),
-		filename: 'bundle.js'
-	}
-}
+  entry: path.join(__dirname, 'client/app/app.js'),
+  output: {
+    path: path.join(__dirname, 'client/app'),
+    filename: 'bundle.js',
+  },
+};
