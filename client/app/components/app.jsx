e2fec9627afee35f054d22fc0fb47cc1fa968f48
melbee <melba@transferhero.com>
2016-11-08 15:46:03 -0800

    [add] added react component

--- client/app/components/app.jsx
+++ client/app/components/app.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-@connect((store) => {
-  return {
-    visData: store.visData,
-  };
-})
+// @connect((store) => {
+//   return {
+//     visData: store.visData,
+//   };
+// })
 
-export default class App extends React.Component {
-  render() {
-    return
-    <div>
-      {this.props}
-    </div>
-  }
-}
-
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
+// export default class App extends React.Component {
 //   render() {
-//     return (
-//      <div>Hello World from app.jsx</div>
-//     );
+//     return
+//     <div>
+//       {this.props}
+//     </div>
 //   }
 // }
 
-// export default App;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+     <div>Hello World from app.jsx</div>
+    );
+  }
+}
+
+export default App;
 
