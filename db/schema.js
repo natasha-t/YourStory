b829de78d7896343b27b2c94d66944b4f13fa450
natasha-t <nthapli@gmail.com>
2016-11-07 19:09:29 -0800

    [refactor] remove database queries

--- db/schema.js
+++ db/schema.js
@@ -38,6 +38,7 @@ const Domain = db.define('domain', {
    type: Sequelize.INTEGER,
    primaryKey: true,
    autoIncrement: true,
+   unique: true,
  },
  domain: Sequelize.STRING,
 })
@@ -63,49 +64,6 @@ db
   .sync({force: false})
   .then(function() {
     console.log('Tables created');
-    //Create user and domain 
-      // User.create({ username: 'nat' }).then(function() {
-     	// console.log('Created new user');
-   	  // });
-      // Domain.create({ domain: 'google' }).then(function() {
-      // 	console.log('created new domain')
-      // });
-
-      // User.create({ username: 'Fred'}).then(function(){
-      // 	console.log('user created');
-      // })
-
-      // Domain.create({ domain: 'facebook' }).then(function(){
-      // 	console.log('created facebook domain')
-      // })
-
-      //  Domain.create({ domain: 'yahoo' }).then(function(){
-      // 	console.log('created yahoo domain')
-      // })
-
-
-
-
-    //Add domain to user
-	    // User.findOne({ where: { username: 'Fred' }, include: [Domain] }).then(function(user){
-	    //   	console.log('FOUND USER', user);
-	    //   	Domain.create({ domain: 'weather' }).then(function(domain){
-	    //   		console.log('FOUND DOMAIN');
-	    //   		user.addDomain(domain, {count: 1}).then(function(){
-	    //   			console.log('added domain to user');
-	    //   		})
-	    //   	})
-
-	    //   })
-
-
-    //get User domains
-	   	User.findOne({ where: {username: 'nat'}, include: [Domain] }).then(function(user){
-			user.getDomains().then(function(domains){
-				console.log('USER DOMAINS', domains);
-			});
-		})
-
  })
   .catch(function(err){
   	console.log(err);
