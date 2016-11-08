ddc70d3a91c081bc4a9a16bc4287675ddaf6fa7b
melbee <melba@transferhero.com>
2016-11-08 15:06:49 -0800

    [feature] started chrome extension popup and background page

---
+++ client/app/components/app.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+     <div>Hello World from app.jsx</div>
+    );
+  }
+}
+
+export default App;
---
+++ client/app/reducers/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import App from './components/app.jsx';
+
+const anchor = document.createElement('div');
+anchor.id = 'yourstory-anchor';
+
+document.body
