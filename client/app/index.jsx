85f72507521e88d53eae576e9c232856e5b2dea0
melbee <melba@transferhero.com>
2016-11-08 20:29:12 -0800

    [fix] merged local chrome extension branch with local master

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import {render} from 'react-dom';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/app';
-// import store from './store';
+import store from './store';
 
 const app = document.getElementById('app');
-const anchor = document.createElement('div');
-anchor.id = 'yourstory-anchor';
 
-document.body.insertBefore(anchor, document.body.childNodes[0]);
-render(<App />, document.getElementById('yourstory-anchor'));
-
-// ReactDOM.render(<Provider store={store}>
-// 	<App />
-// </Provider>, app);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, app);
--- client/app/store.js
+++ client/app/store.js
@@ -1,4 +1,7 @@
 import { createStore } from 'redux';
 import allReducers from './reducers';
 
-export default createStore(allReducers);
+// export default createStore(allReducers);
+export default createStore(allReducers, {
+  visData: "hello from store.js!",
+});
