2d0ac4a4df55b48abb14bfa29c99e028203c40f4
Lizzie Shipton <lizzie.shipton@gmail.com>
2016-11-10 15:14:25 -0800

    [fix] pre-change commit

--- client/auth.js
+++ client/auth.js
@@ -13,6 +13,7 @@ export default function getToken() {
     x.open('GET', 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token);
     x.onload = function () {
       alert(x.response);
+      console.log(token);
       //     axios.post('/api/users', {
       //
       // })
@@ -26,3 +27,5 @@ export default function getToken() {
     x.send();
   });
 }
+ya29.CjKSA5Fkv45G_TZe3y949wAIaBBvFwIGAPCt-JuuWXupC4LlbZ1yPzVi3vOrJafUVasEeg
+ya29.CjKSA5Fkv45G_TZe3y949wAIaBBvFwIGAPCt-JuuWXupC4LlbZ1yPzVi3vOrJafUVasEeg
