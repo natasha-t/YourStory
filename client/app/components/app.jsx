90b0870b41db6588a5cce3c4cc279ff1725728ef
Bruna Enos <bruna_bsm@icloud.com>
2016-11-10 19:09:24 -0800

    (resolve) conflicts on app.jsx

--- client/app/components/app.jsx
+++ client/app/components/app.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import store from '../store'
 import History from './history'
-import { Profile } from './detailed'
 
 @connect((store) => {
   return {
