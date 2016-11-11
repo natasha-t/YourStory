51d1574ef22aa54f19e2d87818112bd6244a2a7a
Bruna Enos <bruna_bsm@icloud.com>
2016-11-10 16:44:25 -0800

    (add)express_session

--- client/app/actions/actions.js
+++ client/app/actions/actions.js
@@ -1,37 +1,35 @@
-import axios from 'axios';
-import { createAction } from 'redux-action';
-import getToken from '../../auth';
+import axios from 'axios'
+import { createAction } from 'redux-action'
+import getToken from '../../auth'
 
-function loadVisDataUponResponse(data) {
+function loadVisDataUponResponse (data) {
   return {
     type: 'FETCH_VIS_DATA',
-    payload: data,
+    payload: data
   }
 }
 
+export default function fetchVisData () {
+  return function (dispatch) {
+    getToken()
+    const microsecondsPerDay = 1000 * 60 * 60 * 24
+    const oneDayAgo = (new Date).getTime() - microsecondsPerDay
 
-export default function fetchVisData() {
-  return function(dispatch) {
-    // getToken();
-    const microsecondsPerDay = 1000 * 60 * 60 * 24;
-    const oneDayAgo = (new Date).getTime() - microsecondsPerDay;
+    chrome.history.search({
+      'text': '', // Return every history item....
+      'startTime': oneDayAgo, // that was accessed less than one week ago.
+    }, (array) => {
+      console.log('chrome history', array)
 
-   chrome.history.search({
-     'text': '',              // Return every history item....
-     'startTime': oneDayAgo, // that was accessed less than one week ago.
-     }, (array) => {
-       console.log('chrome history', array);
-       
       axios({
         method: 'post',
-        url: 'http://yourstory-app.herokuapp.com/api/history',
-        data: {history: array},
+        url: 'http:localhost:3000/api/history',
+        data: {history: array}
       }).then(response => {
-        dispatch(loadVisDataUponResponse(response));
+        dispatch(loadVisDataUponResponse(response))
       })
-   });
+    })
 
-    return null;
-
-  };
+    return null
+  }
 }
--- client/auth.js
+++ client/auth.js
@@ -1,28 +1,28 @@
-// import axios from 'axios';
+// import axios from 'axios'
 
-export default function getToken() {
-
+export default function getToken () {
   chrome.identity.getAuthToken({
-    interactive: true,
+    interactive: true
   }, (token) => {
+    console.log(token)
     if (chrome.runtime.lastError) {
-      alert(chrome.runtime.lastError.message);
-      return;
+      // alert(chrome.runtime.lastError.message)
+      return
     }
-    const x = new XMLHttpRequest();
-    x.open('GET', 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token);
+    const x = new XMLHttpRequest()
+    x.open('GET', 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token)
     x.onload = function () {
-      alert(x.response);
-      //     axios.post('/api/users', {
-      //
-      // })
-      // .then(function (response) {
-      //   console.log(response);
-      // })
-      // .catch(function (error) {
-      //   console.log(error);
-      // });
-    };
-    x.send();
-  });
+      alert(x.response)
+    //     axios.post('/api/users', {
+    //
+    // })
+    // .then(function (response) {
+    //   console.log(response)
+    // })
+    // .catch(function (error) {
+    //   console.log(error)
+    // })
+    }
+    x.send()
+  })
 }
