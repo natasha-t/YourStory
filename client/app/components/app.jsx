093156a0a1934c83c4ba56ef67c051b740d9480e
Lizzie Shipton <lizzie.shipton@gmail.com>
2016-11-12 10:44:54 -0800

    [add] nothing

--- client/app/components/app.jsx
+++ client/app/components/app.jsx
@@ -20,7 +20,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Router history={hashHistory}>
-        <Route path="/" component={Container}>
+        <Route path="/history" component={Container}>
           <IndexRoute component={History} />
         </Route>
       </Router>
