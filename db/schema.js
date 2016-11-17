90a44db70125d1e09b6fd2399c9980705a4704f1
Lizzie Shipton <lizzie.shipton@gmail.com>
2016-11-17 11:25:18 -0800

    [add] creating category array to send to client

--- db/schema.js
+++ db/schema.js
@@ -76,6 +76,73 @@ db
   .sync({ force: false })
   .then(() => {
     console.log('Tables created');
+    // User.create({ username: 'Lizzie' })
+    // .then(() => {
+    //   console.log('user created!');
+    // })
+    //
+    // Domain.create({ domain: 'google.com' })
+    // .then(()=> {
+    //   console.log('domain created');
+    // })
+    //
+    // Domain.create({ domain: 'yahoo.com' })
+    // .then(()=> {
+    //   console.log('domain created');
+    // })
+    //
+    // Domain.create({ domain: 'etsy.com' })
+    // .then(()=> {
+    //   console.log('domain created');
+    // })
+    //
+    // Domain.create({ domain: 'target.com' })
+    // .then(()=> {
+    //   console.log('domain created');
+    // })
+    //
+    // Category.create({ category: 'search'})
+    //
+    // Category.create({ category: 'shopping'})
+
+    // Domain.findOne({ where: { domain: 'google.com' } })
+    // .then((domain) => {
+    //   Category.findOne({ where: { category: 'search' } })
+    //   .then((category) => {
+    //     category.setSites(domain);
+    //   });
+    // });
+  //   Domain.findOne({ where: { domain: 'google.com' } })
+  //   .then((domain) => {
+  //     domain.getCategory()
+  //     .then((category) => {
+  //       console.log(category);
+  //     })
+  //   })
+
+  const catData = [{}];
+
+  User.findOne({ where: { username: 'Lizzie Shipton' } })
+  .then((user) => {
+    user.getDomains()
+      .then((domains) => {
+        for (let i = 0; i < domains.length; i++) {
+          domains[i].getCategory()
+          .then((category) => {
+            for(let j = 0; j < catData.length; j++) {
+              if(!catData[j].category) {
+                catData[j].category = category.dataValues.category;
+                catData[j].domains = [domains[j].dataValues.domain];
+                catData[j].allVisitCount = domains[j].dataValues.users_domains.count;
+              } else {
+                catData[j].domains.push([domains[j].dataValues.domain]);
+                catData[j].allVisitCount += domains[j].dataValues.users_domains.count;
+              }
+            }
+          })
+        }
+     })
+  })
   })
   .catch((err) => {
     console.log(err);
