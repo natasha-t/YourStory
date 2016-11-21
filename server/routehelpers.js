7665a9145b92f3f37fadd70f8d1c11ad518f9072
natasha-t <nthapli@gmail.com>
2016-11-20 18:26:09 -0800

    [refactor] add d3 chart code

--- server/routehelpers.js
+++ server/routehelpers.js
@@ -471,25 +471,25 @@ module.exports = {
 
   getWeekData: (req, res) => {
     const dummyData = [
-      {day: 'Monday',
+      {day: '20161011',
         domains: [{ domain: 'google.com', totalCount: 100 }, { domain: 'stackoverflow.com', totalCount: 50 }, { domain: 'github.com', totalCount: 50 }],
        total: 200},
-      {day: 'Tuesday',
+      {day: '20161012',
         domains: [{ domain: 'google.com', totalCount: 200 }, { domain: 'stackoverflow.com', totalCount: 60 }, { domain: 'github.com', totalCount: 50 }],
        total: 310},
-      {day: 'Wednesday',
+      {day: '20161013',
         domains: [{ domain: 'google.com', totalCount: 250 }, { domain: 'stackoverflow.com', totalCount: 70 }, { domain: 'github.com', totalCount: 50 }],
        total: 370},
-      {day: 'Thursday',
+      {day: '20161014',
         domains: [{ domain: 'google.com', totalCount: 150 }, { domain: 'stackoverflow.com', totalCount: 55 }, { domain: 'github.com', totalCount: 40 }],
        total: 245},
-      {day: 'Friday',
+      {day: '20161015',
         domains: [{ domain: 'google.com', totalCount: 300 }, { domain: 'stackoverflow.com', totalCount: 100 }, { domain: 'github.com', totalCount: 200 }],
        total: 600},
-       {day: 'Saturday',
+       {day: '20161016',
         domains: [{ domain: 'google.com', totalCount: 100 }, { domain: 'stackoverflow.com', totalCount: 20 }, { domain: 'github.com', totalCount: 20 }],
        total: 140},
-      {day: 'Sunday',
+      {day: '20161017',
         domains: [{ domain: 'google.com', totalCount: 35 }, { domain: 'stackoverflow.com', totalCount: 2 }, { domain: 'github.com', totalCount: 10 }],
        total: 47}
     ]
