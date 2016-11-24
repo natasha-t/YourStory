cd1dd72c05f35e58afd6d122ab87c4ed573a5387
natasha-t <nthapli@gmail.com>
2016-11-23 17:03:46 -0800

    [add] add circles to line graph

--- client/app/components/graph.jsx
+++ client/app/components/graph.jsx
@@ -101,40 +101,40 @@ export default class Graph extends React.Component {
 
 
     //======= CREATE SVG ELEMENT =======
-    const svg = d3.select("svg"),
+    const svg = d3.select('svg'),
     margin = { top: 20, right: 80, bottom: 20, left: 50 },
-    width = svg.attr("width") - margin.left - margin.right,
-    height = svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    width = svg.attr('width') - margin.left - margin.right,
+    height = svg.attr('height') - margin.top - margin.bottom,
+    g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 
     //======= CREATE X AND Y SCALES ======
     //12 pm appearing on ticks between days
-    const x = d3.scaleTime().domain([new Date(startDate.year, startDate.month, startDate.date), new Date(endDate.year, endDate.month, endDate.date)]).range([0, width])
-    const y = d3.scaleLinear().domain([min, max]).range([height, 0])
+    const x = d3.scaleTime().domain([new Date(startDate.year, startDate.month, startDate.date), new Date(endDate.year, endDate.month, endDate.date)]).range([0, width]);
+    const y = d3.scaleLinear().domain([min, max]).range([height, 0]);
 
 
     //DRAW X AND Y AXIS
-    g.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
+    g.append('g')
+        .attr('class', 'axis axis--x')
+        .attr('transform', 'translate(0,' + height + ')')
         .call(d3.axisBottom(x))
       .append('text')
-        .attr("x", 40)
-        .attr("y", 30)
-        .attr("dx", "0.71em")
-        .attr("fill", "#000")
-        .text("Days");
+        .attr('x', 40)
+        .attr('y', 30)
+        .attr('dx', '0.71em')
+        .attr('fill', '#000')
+        .text('Days');
 
-    g.append("g")
-        .attr("class", "axis axis--y")
+    g.append('g')
+        .attr('class', 'axis axis--y')
         .call(d3.axisLeft(y))
-      .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("fill", "#000")
-        .text("Visit Count");
+      .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 6)
+        .attr('dy', '0.71em')
+        .attr('fill', '#000')
+        .text('Visit Count');
 
         //======= PER DOMAIN ========
 
@@ -160,13 +160,34 @@ export default class Graph extends React.Component {
 
     //APPEND ALL DOMAINS LINE TO GRAPH
     //+++ add css class to distinguish each line +++
-    const generateSVG = (domain, color) => {
-      return svg.append("path")
-       .attr("d", generateLine()(domain))
-       .attr("stroke", color)
-       .attr("stroke-width", 2)
-       .attr("fill", "none");
+
+    const makeCircle = () => {
+
     }
+
+    const generateSVG = (domain, color) => {
+      svg.append('path')
+       .attr('d', generateLine()(domain))
+       .attr('stroke', color)
+       .attr('stroke-width', 2)
+       .attr('fill', 'none')
+       
+       svg.selectAll('.circle')
+       .data(domain)
+       .enter()
+       .append('svg:circle')
+       .attr('class', 'graph-circle')
+       .attr('fill', color)
+       .attr('r', 3.5)
+       .attr('cx', (d) => {
+         return x(d.date);
+       })
+       .attr('cy', (d) => {
+         return y(d.count);
+       })
+
+       return svg;
+    };
 
 
     const createDomainPath = (domain, color) => {
@@ -174,37 +195,17 @@ export default class Graph extends React.Component {
     }
 
 
-    const allData = () => {
-      return data[0].domains.map((domain) => {
-        console.log('domain', domain.domain);
-        const colors = ['red', 'blue', 'green'];
-        return createDomainPath(domain.domain, Math.floor(Math.random() * colors.length));
-      })
-    }
-
-
     //+++ somehow render all svgs at once by iterating +++
-      console.log('domains array', data[0].domains[1].domain)
-
-
-    if (data[0].domains.length === 1) {
-      createDomainPath(data[0].domains[0].domain, '#E0B1F2');
-    } else if (data[0].domains.length === 2) {
-      createDomainPath(data[0].domains[0].domain, '#E0B1F2');
-      createDomainPath(data[0].domains[1].domain, '#6EBCD4');
-    } else if (data[0].domains.length === 3) {
-      createDomainPath(data[0].domains[0].domain, '#E0B1F2');
-      createDomainPath(data[0].domains[1].domain, '#6EBCD4');
-      createDomainPath(data[0].domains[2].domain, '#89D46E');
+    for (const domain of data[0].domains) {
+     createDomainPath(domain.domain, '#' + Math.floor(Math.random() * 16777215).toString(16));
     }
-
 
 
   }
 
   render() {
     return (
-      <div id="graph"><svg width="960" height="200"></svg></div>
+      <div id='graph'><svg width='960' height='200'></svg></div>
     );
   }
 
